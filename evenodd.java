ee73efe05f186ee3deb20f9d1ab0d987426817a4
Vanshikalamba <48330999+Vanshikalamba@users.noreply.github.com>
2020-10-01 00:59:41 +0530

    first

---
+++ evenodd.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class evenodd
+{
+public static void main(String args[])
+{
+int a;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a no.:");
+a=sc.nextInt();
+if(a%2==0)
+System.out.println("entered no. is even");
+else
+System.out.println("entered no. is odd");
+}
+}
